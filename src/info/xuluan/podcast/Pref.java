dbd85601a9a8ead32cedcccc96e4c3bdbd329fcf
Jim McBeath <android@j.jimmc.org>
2012-11-05 21:00:28 -0800

    Improved Preferences screen: display values, create groups, improve text.

--- src/info/xuluan/podcast/Pref.java
+++ src/info/xuluan/podcast/Pref.java
@@ -16,23 +16,36 @@
 
 package info.xuluan.podcast;
 
-import info.xuluan.podcastj.R;
 import info.xuluan.podcast.service.PodcastService;
-import android.app.Service;
+import info.xuluan.podcastj.R;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.preference.Preference;
 
 public class Pref extends HapiPreferenceActivity {
 
 	public static final String HAPI_PREFS_FILE_NAME = "info.xuluan.podcastj_preferences";
 		//Default filename is our package name (see manifest) with _preferences appended
+	private static final int DAYS_PER_WEEK = 7;
+	private static final int DAYS_PER_MONTH = 30;
+	private static final int DAYS_PER_YEAR = 365;
+	private static final int HOURS_PER_DAY = 24;
+	private static final int HOURS_PER_WEEK = HOURS_PER_DAY * DAYS_PER_WEEK;
+	private static final int HOURS_PER_MONTH = HOURS_PER_DAY * DAYS_PER_MONTH;
+	private static final int HOURS_PER_YEAR = HOURS_PER_DAY * DAYS_PER_YEAR;
+	private static final int MINUTES_PER_HOUR = 60;
+	private static final int MINUTES_PER_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY;
+	private static final int MINUTES_PER_WEEK = MINUTES_PER_HOUR * HOURS_PER_WEEK;
+	
 	private PodcastService serviceBinder = null;
 	ComponentName service = null;
+	private SummaryUpdater summaryUpdater = new SummaryUpdater();
 
 	private ServiceConnection serviceConnection = new ServiceConnection() {
 		public void onServiceConnected(ComponentName className, IBinder service) {
@@ -62,15 +75,18 @@ public class Pref extends HapiPreferenceActivity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-
+		SharedPreferences prefs = getPreferenceScreen().getSharedPreferences();
+		prefs.registerOnSharedPreferenceChangeListener(summaryUpdater);
+		summaryUpdater.updateAllSummaries(prefs);
 	}
 
 	@Override
 	protected void onPause() {
-
 		super.onPause();
 		if(serviceBinder!=null)
 			serviceBinder.updateSetting();
+	    getPreferenceScreen().getSharedPreferences()
+            .unregisterOnSharedPreferenceChangeListener(summaryUpdater);
 	}
 
 	@Override
@@ -78,6 +94,118 @@ public class Pref extends HapiPreferenceActivity {
 		super.onDestroy();
 		unbindService(serviceConnection);
 		// stopService(new Intent(this, service.getClass()));
+	}
+
+	class SummaryUpdater implements OnSharedPreferenceChangeListener {
+		public void updateAllSummaries(SharedPreferences prefs) {
+			String[] keys = {
+					"pref_update_wifi", "pref_update_mobile", "pref_download_only_wifi",
+					"pref_max_new_items",
+					"pref_item_expire", "pref_download_file_expire", "pref_played_file_expire",
+					"pref_rewind_interval", "pref_fast_forward_interval"
+			};
+			for (String key : keys) {
+				onSharedPreferenceChanged(prefs, key);
+			}
+		}
+		
+	    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+	    	if (key.equals("pref_download_only_wifi"))
+	    		updateDisableMobile(prefs,key);
+	        if (key.equals("pref_update_wifi"))
+	        	updateMinuteIntervalSummary(prefs, key);
+	        else if (key.equals("pref_update_mobile"))
+	        	updateMinuteIntervalSummary(prefs, key);
+	        else if (key.equals("pref_max_new_items"))
+	        	updateMaxItems(prefs, key);
+	        else if (key.equals("pref_item_expire"))
+	        	updateExpireDaysSummary(prefs, key);
+	        else if (key.equals("pref_download_file_expire"))
+	        	updateExpireDaysSummary(prefs, key);
+	        else if (key.equals("pref_played_file_expire"))
+	        	updateExpireHoursSummary(prefs, key);
+	        else if (key.equals("pref_rewind_interval"))
+	        	updatePlayerIntervalSummary(prefs, key, "Rewind");
+	        else if (key.equals("pref_fast_forward_interval"))
+	        	updatePlayerIntervalSummary(prefs, key, "Fast-forward");
+	    }
+
+	    private void updateDisableMobile(SharedPreferences prefs, String key) {
+	    	Boolean disabled = prefs.getBoolean(key, false);
+	    	Preference mobileIntervalPref = findPreference("pref_update_mobile");
+	    	mobileIntervalPref.setEnabled(!disabled);
+	    	if (disabled)
+	    		mobileIntervalPref.setSummary("To set, enable Mobile update");
+	    	else
+	    		onSharedPreferenceChanged(prefs, "pref_update_mobile");
+	    }
+	    
+	    private void updateMaxItems(SharedPreferences prefs, String key) {
+	    	Preference pref = findPreference(key);
+	    	String value = prefs.getString(key, "");
+	    	pref.setSummary("Download up to "+value+" new items");
+	    }
+	    
+	    private void updateExpireDaysSummary(SharedPreferences prefs, String key) {
+            Preference pref = findPreference(key);
+            String value = prefs.getString(key,"");
+            int days = Integer.parseInt(value);
+            String s;
+            if ((days%DAYS_PER_YEAR)==0)
+            	s = maybePlural(days/DAYS_PER_YEAR,"year","years");
+            else if ((days%DAYS_PER_MONTH)==0)
+            	s = maybePlural(days/DAYS_PER_MONTH,"month","months");
+            else if ((days%DAYS_PER_WEEK)==0)
+            	s = maybePlural(days/DAYS_PER_WEEK,"week","weeks");
+            else
+            	s = maybePlural(days,"day","days");
+            pref.setSummary("Expire after "+s);	    	
+	    }
+	    
+	    private void updateExpireHoursSummary(SharedPreferences prefs, String key) {
+            Preference pref = findPreference(key);
+            String value = prefs.getString(key,"");
+            int hours = Integer.parseInt(value);
+            String s;
+            if ((hours%HOURS_PER_YEAR)==0)
+            	s = maybePlural(hours/HOURS_PER_YEAR,"year","years");
+            else if ((hours%HOURS_PER_MONTH)==0)
+            	s = maybePlural(hours/HOURS_PER_MONTH,"month","months");
+            else if ((hours%HOURS_PER_WEEK)==0)
+            	s = maybePlural(hours/HOURS_PER_WEEK,"week","weeks");
+            else if ((hours%HOURS_PER_DAY)==0)
+            	s = maybePlural(hours/HOURS_PER_DAY,"day","days");
+            else
+            	s = maybePlural(hours,"hour","hours");
+            pref.setSummary("Expire after "+s);	    	
+	    }
+	    
+	    private void updateMinuteIntervalSummary(SharedPreferences prefs, String key) {
+            Preference pref = findPreference(key);
+            String value = prefs.getString(key,"");
+            int minutes = Integer.parseInt(value);
+            String s;
+            if ((minutes%MINUTES_PER_WEEK)==0)
+            	s = maybePlural(minutes/MINUTES_PER_WEEK,"week","weeks");
+            else if ((minutes%MINUTES_PER_DAY)==0)
+            	s = maybePlural(minutes/MINUTES_PER_DAY,"day","days");
+            else if ((minutes%MINUTES_PER_HOUR)==0)
+            	s = maybePlural(minutes/MINUTES_PER_HOUR,"hour","hours");
+            else
+            	s = maybePlural(minutes,"minute","minutes");
+            pref.setSummary("Every "+s);	    	
+	    }
+	    
+	    private String maybePlural(int n, String singular, String plural) {
+	    	return ""+n+" "+((n==1)?singular:plural);
+	    }
+
+	    private void updatePlayerIntervalSummary(SharedPreferences prefs, String key, String direction) {
+            Preference pref = findPreference(key);
+            String value = prefs.getString(key,"");
+            pref.setSummary(direction+" "+value+" seconds");	    	
+	    }
+
 	}
 
 }
